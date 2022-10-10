bd834f445a7175351f2be8ad2020165427b68832
Sergey_Korotich <autodiller@bk.ru>
2022-10-10 22:38:51 +0500

    first commit

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -6,6 +6,7 @@ import javax.persistence.Table;
 
 @Table
 public class User {
+
     @Id
     private Long id;
 
@@ -58,5 +59,10 @@ public class User {
 
     public void setAge(Byte age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User {id = '%s', name = '%s', lastName = '%s', age = '%d'}", getId(), getName(), getLastName(), getAge());
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,42 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection conn;
+    private static Util instance;
+    private static final String URL = "jdbc:mysql://localhost:3307/users";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            return conn = DriverManager.getConnection(URL,USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+   public  static Util getInstance() throws SQLException {
+       if (instance == null) {
+           synchronized (Util.class) {
+               if (instance == null) {
+                   instance = new Util();
+               }
+           }
+       }
+       return instance;
+   }
+
+
+
 }
